d36e04d007d1764cd13c3d208b84b1b2b215ab58
Andre Lemos <andretlemos@gmail.com>
2024-05-13 16:43:42 -0300

    Adding scrapli send command and save output

---
+++ scrapli_send_command_all_save_output.py
@@ -0,0 +1,79 @@
+from scrapli.driver.core import AsyncJunosDriver
+from getpass import getpass
+import pynetbox
+from rich import print
+import asyncio
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+import aiofiles
+
+
+now = datetime.now()
+date = now.strftime("%Y-%m-%d_%H:%M:%S")
+
+file = "outputs/send_commands_" + date + ".txt"
+
+
+load_dotenv()
+
+nb_url = os.getenv("NETBOX_URL")
+nb_token = os.getenv("NETBOX_TOKEN")
+
+username = input("Digite o seu nome de usuario: ")
+passwd = getpass("Digite seua senha: ")
+#command = "show version"
+command = input("Digite o comando a ser enviado: ")
+
+parametros_comuns = {
+    "auth_username": username,
+    "auth_password": passwd,
+    "auth_strict_key": False,
+    "transport": "asyncssh",
+    }
+
+#router = input("Digite o roteador para coletar as informacoes: ").upper()
+#command = open("commands.txt", "r")
+nb = pynetbox.api(nb_url,token=nb_token, threading=True)
+devices = nb.dcim.devices.filter(role="core", status="active")
+
+routers = {}
+
+
+async def connect_and_execute(router):
+    router_ip = routers[router]
+    device =  {"host": router_ip, **parametros_comuns }
+    async with AsyncJunosDriver(**device) as conn:
+        response = await conn.send_command(command)
+           
+        async with aiofiles.open(file, "a") as myfile:
+            await myfile.write(f"### Enviando o comando para o roteador { router } ####\n ")   
+            await myfile.write(response.result)
+
+        print(f"### Enviando o comando para o roteador { router } ####\n ")
+        print(response.result)
+
+async def main():
+
+    for device in devices:
+        if str(device.tags) == "[CORE]":
+            ip = str(device.primary_ip)
+            ip = ip.split("/")[0]
+            router = device.name
+           #Adicionando o nome do roteador com seu ip no dicionário
+            routers[router] = ip
+
+    tasks = [connect_and_execute(router) for router in routers]
+    try:
+        await asyncio.gather(*tasks)
+    except:
+        print(f"Não foi possível acessar o roteador {router}")
+          
+        async with aiofiles.open(file, "a") as myfile:
+            await myfile.write(f"Não foi possível acessar o roteador {router}")
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+    
+
